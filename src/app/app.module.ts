f0dbe652cce67a1b40a88436fce552640f375c51
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-07 19:03:45 -0300

    import mat card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardProjectComponent } from './component/card-project/card-project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    CardProjectComponent
+  ],
+  exports: [
+    MatCardModule,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
