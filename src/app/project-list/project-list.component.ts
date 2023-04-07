7f3ba18cbac33795651bab7457be4f131f74615e
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-07 19:27:58 -0300

    card-project

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -7,23 +7,47 @@ import { Octokit } from '@octokit/rest';
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
+  allProjects: any[] = [];
+  projects: any[] = [];
+  showMore = false;
+  searchQuery = '';
   octokit = new Octokit(
     //{auth: 'f514a0ba2836d3c6ae1fa637e3e36c2abc267919'} verificar
   );
-  projects: any[] | undefined;
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() { }
+  ngOnInit() {
+    // Substitua "seu_nome_de_usuario" abaixo pelo seu nome de usuário do GitHub
 
     this.octokit.repos.listForUser({
-      username: 'AllanGaiteiro'
+      username: 'AllanGaiteiro',
+
     }).then(({ data }) => {
       // exibe os dados dos seus projetos do GitHub no console
-      console.log(data);
-      this.projects = data
+      this.allProjects = data;
+      this.projects = data.slice(0, 6);
+      console.log(this.projects)
     }).catch(error => {
       console.error(error); // exibe uma mensagem de erro no console se a chamada da API falhar
     });
+
+  }
+
+  toggleShowMore() {
+    this.showMore = !this.showMore;
+    if (this.showMore) {
+      this.projects = this.allProjects;
+    } else {
+      this.projects = this.allProjects.slice(0, 6);
+    }
+  }
+
+  searchProjects() {
+    console.log(this.searchQuery)
+
+    this.projects = this.allProjects.filter((project) => {
+      return project.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+    }).slice(0, 6);
   }
 
 }
