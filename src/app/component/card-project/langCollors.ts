19770dbfd565ef3a44f02fbd64d7eb99ca9a9ea1
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-08 02:31:32 -0300

    criado o ennum de cores para cada linguagem

---
+++ src/app/component/card-project/langCollors.ts
@@ -0,0 +1,9 @@
+
+export enum langCollors {
+    "TypeScript" = "#3178C6",
+    "Java" = "#B07219",
+    "HTML" = "#E34C26",
+    "SCSS" = "#0D1117",
+    "JavaScript" = "#F1E05A",
+    "CSS" = "#563D7C"
+}
