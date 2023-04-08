400016c046dd8407564814d715c8c3bb933a6fdf
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-08 17:04:02 -0300

    add nuw languagens

--- src/app/component/card-project/langCollors.ts
+++ src/app/component/card-project/langCollors.ts
@@ -5,5 +5,12 @@ export enum langCollors {
     "HTML" = "#E34C26",
     "SCSS" = "#0D1117",
     "JavaScript" = "#F1E05A",
-    "CSS" = "#563D7C"
+    "CSS" = "#563D7C",
+    "C++" = "#F34B7D",
+    "C" = "#555555",
+    "Handlebars" = "#F7931E",
+    "Dart" = "#00B4AB",
+    "Swift" = "#F05138",
+    "Kotlin" = "#A97BFF",
+    "Objective-C" = "#438EFF",
 }
